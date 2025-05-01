41a87753e6d92be762cbdef185fe9131405a1d52
Arfazrll <azril4974@gmail.com>
2025-05-01 16:12:25 +0700

    deletecache

--- BackEnd/dist/app.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// This file serves as the entry point for the application
-// The actual implementation is in server.ts for better organization
-require("./server");
-//# sourceMappingURL=app.js.map
